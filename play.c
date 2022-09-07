04d567128141da43e02feec03d60eb6a1f29cbea
Dylan Thinnes <dylan.thinnes@protonmail.com>
2022-09-07 22:14:04 +0100

    Add multi_channel mode
    Remove printf, memory map /dev/fb0 for 800x600 projector

--- play.c
+++ play.c
@@ -11,34 +11,26 @@ int active_buffer = 0;
 int buffer_width = 800;
 int buffer_height = 600;
 int buffer[2][3][800][600];
+int multi_channel = 0;
 
-int (*mem)[1920];
+int (*mem)[800];
 
-void randomize_rgb () {
-  for (int channel = 0; channel < 3; channel++) {
-    for (int xx = 0; xx < buffer_width; xx++) {
-      for (int yy = 0; yy < buffer_height; yy++) {
+void randomize () {
+  for (int xx = 0; xx < buffer_width; xx++) {
+    for (int yy = 0; yy < buffer_height; yy++) {
+      int channel_count = multi_channel ? 3 : 1;
+      for (int channel = 0; channel < channel_count; channel++) {
         buffer[active_buffer][channel][xx][yy] = rand() % 2;
       }
     }
   }
 }
 
-void randomize () {
+void step_state () {
   for (int xx = 0; xx < buffer_width; xx++) {
     for (int yy = 0; yy < buffer_height; yy++) {
-      int v = rand() % 2;
-      for (int channel = 0; channel < 3; channel++) {
-        buffer[active_buffer][channel][xx][yy] = v;
-      }
-    }
-  }
-}
-
-void step_state () {
-  for (int channel = 0; channel < 3; channel++) {
-    for (int xx = 0; xx < buffer_width; xx++) {
-      for (int yy = 0; yy < buffer_height; yy++) {
+      int channel_count = multi_channel ? 3 : 1;
+      for (int channel = 0; channel < channel_count; channel++) {
         int neighbours = 0;
         for (int dx = -1; dx <= 1; dx++) {
           for (int dy = -1; dy <= 1; dy++) {
@@ -57,13 +49,17 @@ void step_state () {
 }
 
 void redraw () {
-  for (int channel = 0; channel < 3; channel++) {
-    for (int xx = 0; xx < buffer_width; xx++) {
-      for (int yy = 0; yy < buffer_height; yy++) {
-        mem[yy][xx] =
-          (buffer[active_buffer][0][xx][yy] * 0x00ff0000) |
-          (buffer[active_buffer][1][xx][yy] * 0x0000ff00) |
-          (buffer[active_buffer][2][xx][yy] * 0x000000ff);
+  for (int xx = 0; xx < buffer_width; xx++) {
+    for (int yy = 0; yy < buffer_height; yy++) {
+      if (multi_channel) {
+        for (int channel = 0; channel < 3; channel++) {
+          mem[yy][xx] =
+            (buffer[active_buffer][0][xx][yy] * 0x00ff0000) |
+            (buffer[active_buffer][1][xx][yy] * 0x0000ff00) |
+            (buffer[active_buffer][2][xx][yy] * 0x000000ff);
+        }
+      } else {
+        mem[yy][xx] = buffer[active_buffer][0][xx][yy] ? 0x00ffffff : 0x00000000;
       }
     }
   }
@@ -72,20 +68,28 @@ void redraw () {
 void init_from_fb () {
   for (int xx = 0; xx < buffer_width; xx++) {
     for (int yy = 0; yy < buffer_height; yy++) {
-      int val = mem[yy][xx];
-      int r = (val >> 16) & 0xff;
-      int g = (val >>  8) & 0xff;
-      int b = (val >>  0) & 0xff;
-      buffer[active_buffer][0][xx][yy] = r > 16;
-      buffer[active_buffer][1][xx][yy] = g > 16;
-      buffer[active_buffer][2][xx][yy] = b > 16;
+      if (multi_channel) {
+        int val = mem[yy][xx];
+        int r = (val >> 16) & 0xff;
+        int g = (val >>  8) & 0xff;
+        int b = (val >>  0) & 0xff;
+        buffer[active_buffer][0][xx][yy] = r > 16;
+        buffer[active_buffer][1][xx][yy] = g > 16;
+        buffer[active_buffer][2][xx][yy] = b > 16;
+      } else {
+        int val = mem[yy][xx];
+        int r = (val >> 16) & 0xff;
+        int g = (val >>  8) & 0xff;
+        int b = (val >>  0) & 0xff;
+        buffer[active_buffer][0][xx][yy] = r > 16 || g > 16 || b > 16;
+      }
     }
   }
 }
 
 int main (int argc, char **argv) {
   int fd = open("/dev/fb0", O_RDWR);
-  mem = (int (*)[1920]) mmap(NULL, 1920 * 1080 * 4, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+  mem = (int (*)[800]) mmap(NULL, 800 * 600 * 4, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
   /*
   for (int xx = 0; xx < 256; xx++) {
     for (int yy = 0; yy < 256; yy++) {
@@ -96,7 +100,7 @@ int main (int argc, char **argv) {
   }
   */
 
-  init_from_fb();
+  randomize();
 
   uint64_t elapsed = 0;
   uint64_t interval = 100000000L;
@@ -111,14 +115,14 @@ int main (int argc, char **argv) {
     elapsed += req.tv_nsec - res.tv_nsec;
 
     if (elapsed > interval) {
-      printf("elapsed %d\n", elapsed);
+      //printf("elapsed %d\n", elapsed);
       elapsed -= interval;
-      printf("redraw\n");
+      //printf("redraw\n");
       step_state();
       redraw();
     }
   }
   redraw();
 
-  munmap(mem, 1920 * 1080 * 4);
+  munmap(mem, 800 * 600 * 4);
 }
